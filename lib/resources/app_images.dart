655a2bf740ca47bf8c2cfe8272fbad104d56e2dd
themihabyte <mihabayt@gmail.com>
2022-03-30 16:24:35 +0300

    Placeholder images

--- lib/resources/app_images.dart
+++ lib/resources/app_images.dart
@@ -3,5 +3,11 @@ part of 'resources.dart';
 class AppImages {
   AppImages._();
 
+  static const String bjorkPost = 'images/bjork_post.png';
   static const String imagePlaceholder = 'images/image_placeholder.jpg';
+  static const String macDemarco = 'images/mac_demarco.jpg';
+  static const String macDemarcoAlbumThisOldDog =
+      'images/mac_demarco_album_this_old_dog.jpg';
+  static const String megadeth = 'images/megadeth.jpg';
+  static const String metallica = 'images/metallica.jpg';
 }
---
+++ lib/widgets/entities/list_item.dart
@@ -0,0 +1,12 @@
+abstract class ListItem {
+  final int id;
+  final String imageName;
+  final String title;
+  late final String description;
+
+  ListItem(
+      this.id,
+      this.imageName,
+      this.title,
+      );
+}
